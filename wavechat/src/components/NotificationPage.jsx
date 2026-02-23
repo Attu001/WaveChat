f4883098a289665edf80151075ffc0f8df2e05e6
atish chavan <atish.chavan@64-squares.com>
2026-02-23 15:30:19 +0530

    added url for niotification page

--- wavechat/src/components/NotificationPage.jsx
+++ wavechat/src/components/NotificationPage.jsx
@@ -1,15 +1,30 @@
-import React, { useEffect, useRef } from "react";
+import React, { use, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addNotification } from "../slices/notificationSlice";
 // import {notificationaudio} from "WaveChat/wavechat/public/notification.wav"
+import { notifications } from "../api/services/userServices";
+import { useState } from "react";
 
 const NotificationPage = () => {
-  const userId = localStorage.getItem("id");
-  const { notifications } = useSelector((state) => state.notification);
+
+  // const { notifications } = useSelector((state) => state.notification);
   const dispatch = useDispatch();
+  const [notificationsList,setNotificationsList] = useState([]);
 
   const audioRef = useRef(null);
- 
+  
+  const fetchNotifications = async () => {
+    try {
+      const response = await notifications();
+      setNotificationsList(response.data);
+    } catch (error) {
+      console.error("Error fetching notifications:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchNotifications();
+  },[]);
 
 
   return (
@@ -19,17 +34,17 @@ const NotificationPage = () => {
       <div className="w-full max-w-2xl">
         <h2 className="text-2xl font-semibold mb-6">Notifications</h2>
 
-        {notifications.length === 0 ? (
+        {notificationsList.length === 0 ? (
           <p>No notifications</p>
         ) : (
-          notifications.map((n, index) => (
+          notificationsList.map((n, index) => (
             <div
               key={index}
               className="bg-white p-4 rounded shadow mb-3"
             >
               <h4 className="font-semibold">New Message</h4>
               <p>{n.message}</p>
-              <span className="text-xs text-gray-400">{n.time}</span>
+              <span className="text-xs text-gray-400">{n.created_at}</span>
             </div>
           ))
         )}
